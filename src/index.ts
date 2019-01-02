b2a9a97f3eb3b26cfe32690cc26dde69ac29c756
Jun Kato <i@junkato.jp>
2019-01-03 00:36:17 +0900

    PDF parser starts working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { inspector } from "eyes";
 import * as pdfExtract from "pdf-extract";
 
-// import * as fs from "fs";
-// import * as pdf from "pdf-parse";
-
 let argv = process.argv;
 // argv[0]: node
 // argv[1]: index.js
@@ -13,92 +10,42 @@ if (argv.length < 2) {
   process.exit(1);
 }
 
-// let dataBuffer = fs.readFileSync(argv[2]);
-
-// let options = {
-//   // pdf.js version to use
-//   version: "v2.0.550",
-
-//   // a callback function to parse text and construct data.text
-//   pagerender: (pageData: any) => {
-//     // check documents https://mozilla.github.io/pdf.js/
-//     let renderOptions = {
-//       // replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
-//       normalizeWhitespace: true,
-//       // do not attempt to combine same line TextItem's. The default value is `false`.
-//       disableCombineTextItems: false
-//     };
-
-//     return pageData.getTextContent(renderOptions).then(function(textContent) {
-//       let lastY,
-//         text = "";
-//       for (let item of textContent.items) {
-//         if (lastY == item.transform[5] || !lastY) {
-//           text += item.str;
-//         } else {
-//           text += "\n" + item.str;
-//         }
-//         lastY = item.transform[5];
-//       }
-//       return text;
-//     });
-//   }
-// };
-
-// pdf(dataBuffer, options).then(function(data) {
-//   console.log(data);
-//   // number of pages: 3
-//   // console.log(data.numpages);
-
-//   // number of rendered pages: 3
-//   // console.log(data.numrender);
-
-//   // PDF info:
-//   // { PDFFormatVersion: '1.4',
-//   //   IsAcroFormPresent: false,
-//   //   IsXFAPresent: false,
-//   //   Title: '利用明細書',
-//   //   Author: 'svf',
-//   //   Creator:
-//   //    'SVF for Java Print 9.2 (Revision 9.2.0.38 build 201403061545)',
-//   //   Producer: 'SVF for Java Print',
-//   //   CreationDate: '20181231174500+09\'00\'' }
-//   // console.log(data.info);
-
-//   // PDF metadata
-//   // null
-//   // console.log(data.metadata);
-
-//   // PDF.js version: 1.10.100
-//   // check https://mozilla.github.io/pdf.js/getting_started/
-//   // console.log(data.version);
-
-//   // PDF text
-//   // console.log(data.text);
-// });
-
 let inspect = inspector({ maxLength: 20000 });
 let options = {
   type: "text" // extract the actual text in the pdf file
 };
 
-function parsePDF(file: string, callback: Function) {
-  let processor = pdfExtract(file, options, function(err) {
+interface PDFDataIface {
+  hash: string;
+  text_pages: string[];
+  pdf_path: string;
+  single_page_pdf_file_paths: string[];
+}
+
+function parsePDF(
+  file: string,
+  callback: (err: any, pages?: string[]) => void
+) {
+  let processor = pdfExtract(file, options, (err: any) => {
     if (err) {
       return callback(err);
     }
   });
-  processor.on("complete", function(data) {
-    inspect(data.text_pages, "extracted text pages");
+  processor.on("complete", (data: PDFDataIface) => {
+    // inspect(data, "extracted text pages");
     callback(null, data.text_pages);
   });
-  processor.on("error", function(err) {
-    inspect(err, "error while extracting pages");
+  processor.on("error", (err: any) => {
+    // inspect(err, "error while extracting pages");
     return callback(err);
   });
 }
 
-parsePDF(argv[2], (err, text) => {
+parsePDF(argv[2], (err, pages) => {
   if (err) return console.error(err);
-  console.log(text);
+  pages.forEach((page, i) => {
+    let lines = page.split(/[\015\n]+/g);
+    lines = lines.filter(line => /^[0-9]{2}\/[0-9]{2}\/[0-9]{2}/g.test(line));
+    inspect(lines, `extracted text page: ${i}`);
+  });
 });
